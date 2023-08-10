7a376ab80117dd351c39175a4c7633baa623d448
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-08-10 10:35:17 +0300

    edited button

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,4 +1,5 @@
 <template>
-  <button
-      class="px-[13px] py-[13px] rounded-full border-custom-red border-[2px] text-[20px] xl:px-[35px]">связаться с нами</button>
+  <a
+      href="#contacts"
+      class="text-center px-[13px] py-[13px] rounded-full border-custom-red border-[2px] text-[20px] xl:px-[35px]">связаться с нами</a>
 </template>
