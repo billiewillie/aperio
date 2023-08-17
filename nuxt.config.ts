b0da7529ac8a0b7d217aad0a39255ae90f8e45f9
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-08-17 17:05:36 +0300

    added metatags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,21 @@ export default defineNuxtConfig({
             tailwindcss: {},
             autoprefixer: {},
         },
+    },
+    app: {
+        head: {
+            title: 'Leica Aperio – сканирующие микроскопы для гистологии и цитологии',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Сканирующие микроскопы Leica Aperio - высокопроизводительные системы для цифровой патологии: получение второго мнения в сложных случаях, телемедицина, интеграция в ЛИС, создание электронного архива препаратов.'
+                },
+                {
+                    name: 'keywords',
+                    content: 'Aperio, GT 450 DX, Aperio CS2, ScanScope, Aperio AT2, гистосканер, гистоскан, KFBIO, Pannoramic, 3DHistech, Hamamatsu, Nanozoomer, сканер Leica, сканер Лейка, цифровая патология, сканер стекол, сканирование препаратов, цифровая патология, Digital pathology, аперио, патоморфология, второе мнение'
+                }
+            ],
+
+        }
     }
 })
